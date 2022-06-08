80863d3f2e2cf009f770b1aae87857a71c2b4e33
s3minhtruong <minh.truong@s3corp.com.vn>
2022-06-08 11:36:32 +0700

    Add main slider

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Slider;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,8 +13,9 @@ use Illuminate\Support\Facades\Hash;
 class IndexController extends Controller
 {
     public function index() {
+        $sliders = Slider::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get();
         $categories = Category::orderBy('category_name_en', 'ASC')->get();
-        return view('frontend.index', compact('categories'));
+        return view('frontend.index', compact('categories', 'sliders'));
     }
 
     public function userLogout() {
--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static latest()
  * @method static insert(array $array)
  * @method static findOrFail($brandId)
+ * @method static where(string $string, int $int)
  */
 class Slider extends Model
 {
