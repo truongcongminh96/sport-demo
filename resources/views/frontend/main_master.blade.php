dd28336c08d6fd42e3fd7dab75b96cefc555271b
s3minhtruong <minh.truong@s3corp.com.vn>
2022-06-21 11:32:16 +0700

    Add package cart and modal cart design

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -86,7 +86,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"><span id="pname"></span></h5>
+                <h5 class="modal-title" id="exampleModalLabel"><strong><span id="pname"></span></strong></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -95,41 +95,37 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="..." alt="Card image cap" style="height: 200px; width: 180px" id="pimage">
+                            <img class="card-img-top" src="" alt="Card image cap" style="height: 200px; width: 200px" id="pimage">
 
                         </div>
                     </div>
 
                     <div class="col-md-4">
                         <ul class="list-group">
-                            <li class="list-group-item">Product Price: <strong id="price"></strong></li>
+                            <li class="list-group-item">Product Price: <strong class="text-danger"><span id="pprice"></span></strong><br><del id="oldprice"></del></li>
                             <li class="list-group-item">Product Code: <strong id="pcode"></strong></li>
                             <li class="list-group-item">Category: <strong id="pcategory"></strong></li>
                             <li class="list-group-item">Brand: <strong id="pbrand"></strong></li>
-                            <li class="list-group-item">Stock <strong id="price"></strong></li>
+                            <li class="list-group-item">
+                                Stock:
+                                <span class="badge badge-pill badge-success" id="available" style="background: green; color: white;"></span>
+                                <span class="badge badge-pill badge-danger" id="stockout" style="background: red; color: white;"></span>
+                            </li>
                         </ul>
                     </div>
 
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Choose Color</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
+                            <select class="form-control" id="exampleFormControlSelect1" name="color">
+
                             </select>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group" id="sizeArea">
                             <label for="exampleFormControlSelect1">Choose Size</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
+                            <select class="form-control" id="exampleFormControlSelect1" name="size">
+
                             </select>
                         </div>
 
@@ -168,6 +164,40 @@
                 $('#pcategory').text(data.product.category.category_name_en);
                 $('#pbrand').text(data.product.brand.brand_name_en);
                 $('#pimage').attr('src', '/' + data.product.product_thumbnail);
+
+                if (data.product.discount_price === null) {
+                    $('#pprice').text('');
+                    $('#oldprice').text('');
+                    $('#pprice').text(data.product.selling_price);
+                } else {
+                    $('#pprice').text(data.product.discount_price);
+                    $('#oldprice').text(data.product.selling_price);
+                }
+
+                if (data.product.product_qty > 0) {
+                    $('#available').text('');
+                    $('#stockout').text('');
+                    $('#available').text('available');
+                } else {
+                    $('#available').text('');
+                    $('#stockout').text('');
+                    $('#stockout').text('stockout');
+                }
+
+                $('select[name="color"]').empty();
+                $.each(data.color, function (key, value) {
+                    $('select[name="color"]').append('<option value=" '+value+' ">'+value+'</option>');
+                });
+
+                $('select[name="size"]').empty();
+                $.each(data.size, function (key, value) {
+                    $('select[name="size"]').append('<option value=" '+value+' ">'+value+'</option>');
+                    if (data.size === "") {
+                        $('#sizeArea').hide();
+                    } else {
+                        $('#sizeArea').show();
+                    }
+                });
             }
         });
     }
